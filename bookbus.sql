6ab5968d17db89137f8db3c020627b558b98dba3
16110094-NguyenThiDieuHuyen <nguyenthidieuhuyenpy@gmail.com>
2019-08-13 12:20:42 +0700

    //comit

---
+++ bookbus.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th8 13, 2019 lúc 07:19 AM
+-- Phiên bản máy phục vụ: 10.1.37-MariaDB
+-- Phiên bản PHP: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `bookbus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `chuyendi`
+--
+
+CREATE TABLE `chuyendi` (
+  `id` int(10) NOT NULL,
+  `MaTuyen` varchar(20) NOT NULL,
+  `Gio` time NOT NULL,
+  `DonGia` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `CMND` varchar(11) NOT NULL,
+  `HoTen` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `SDT` varchar(11) NOT NULL,
+  `Email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `khachhang`
+--
+
+INSERT INTO `khachhang` (`CMND`, `HoTen`, `SDT`, `Email`) VALUES
+('221462795', 'Nguyen Trinh', '012345689', 'trinhnguyen@email.com');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhanvien`
+--
+
+CREATE TABLE `nhanvien` (
+  `Id` int(10) NOT NULL,
+  `HoTen` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `SDT` varchar(11) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `ChucVu` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhanvien`
+--
+
+INSERT INTO `nhanvien` (`Id`, `HoTen`, `SDT`, `Email`, `ChucVu`) VALUES
+(1, 'Nguyen Huyen', '0123456789', 'nguyenthidieuhuyenpy@gmail.com', '');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `taikhoan`
+--
+
+CREATE TABLE `taikhoan` (
+  `Email` varchar(50) NOT NULL,
+  `Pass` varchar(20) NOT NULL,
+  `Role` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Đang đổ dữ liệu cho bảng `taikhoan`
+--
+
+INSERT INTO `taikhoan` (`Email`, `Pass`, `Role`) VALUES
+('nguyenthidieuhuyenpy@gmail.com', '123456789', 'nhanvien'),
+('trinhnguyen@email.com', '123456789', 'khachhang');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tuyendi`
+--
+
+CREATE TABLE `tuyendi` (
+  `Id` varchar(20) NOT NULL,
+  `DiaDiemDi` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `DiaDienDen` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `vexe`
+--
+
+CREATE TABLE `vexe` (
+  `CMND` varchar(11) NOT NULL,
+  `IdChuyen` int(10) NOT NULL,
+  `NgayDi` date NOT NULL,
+  `GioDi` time NOT NULL,
+  `HangDoi` varchar(5) NOT NULL,
+  `MaGhe` varchar(5) NOT NULL,
+  `DonGia` decimal(10,0) NOT NULL,
+  `NgayGioDat` datetime NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `chuyendi`
+--
+ALTER TABLE `chuyendi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `MaTuyen` (`MaTuyen`);
+
+--
+-- Chỉ mục cho bảng `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`CMND`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Chỉ mục cho bảng `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Chỉ mục cho bảng `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD PRIMARY KEY (`Email`),
+  ADD KEY `Email` (`Email`);
+
+--
+-- Chỉ mục cho bảng `tuyendi`
+--
+ALTER TABLE `tuyendi`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Chỉ mục cho bảng `vexe`
+--
+ALTER TABLE `vexe`
+  ADD PRIMARY KEY (`CMND`,`IdChuyen`,`HangDoi`,`MaGhe`),
+  ADD KEY `CMND` (`CMND`),
+  ADD KEY `IdChuyen` (`IdChuyen`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `chuyendi`
+--
+ALTER TABLE `chuyendi`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `nhanvien`
+--
+ALTER TABLE `nhanvien`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `chuyendi`
+--
+ALTER TABLE `chuyendi`
+  ADD CONSTRAINT `Foreign_key_CD_TD` FOREIGN KEY (`MaTuyen`) REFERENCES `tuyendi` (`Id`);
+
+--
+-- Các ràng buộc cho bảng `vexe`
+--
+ALTER TABLE `vexe`
+  ADD CONSTRAINT `Foreign_key_VX_CD` FOREIGN KEY (`IdChuyen`) REFERENCES `chuyendi` (`id`),
+  ADD CONSTRAINT `Foreign_key_VX_KH` FOREIGN KEY (`CMND`) REFERENCES `khachhang` (`CMND`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
